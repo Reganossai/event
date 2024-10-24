5bca7bcb0b37272bb7c019902111466e69efeb41
Reagan <ossaireagano@gmail.com>
2024-10-24 09:57:16 +0100

    first commit

---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Landingpage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="flex-div">
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://images.unsplash.com/photo-1542189800-ffe910d48d28?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbnxlbnwwfHwwfHx8MA%3D%3D"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#!" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://images.unsplash.com/photo-1542189800-ffe910d48d28?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbnxlbnwwfHwwfHx8MA%3D%3D"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#!" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://images.unsplash.com/photo-1542189800-ffe910d48d28?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbnxlbnwwfHwwfHx8MA%3D%3D"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#!" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://images.unsplash.com/photo-1542189800-ffe910d48d28?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbnxlbnwwfHwwfHx8MA%3D%3D"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#!" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+
+      
+      </div>
+    </div>
+  );
+};
+
+export default Landingpage;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAddressCard, faBars, faChartSimple, faCircleInfo, faClockRotateLeft, faFutbol, faSuitcase } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faArrowAltCircleUp } from "@fortawesome/free-regular-svg-icons";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [isSlidIn, setIsSlidIn] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    setIsSlidIn(!isSlidIn);
+  };
+
+  nav
+    ? (document.body.style.overflow = "hidden")
+    : (document.body.style.overflow = "auto");
+  return (
+    <nav className="navbar-container">
+      <Link to="/"> <img src="kfkfk.jpg" className="logo" alt="logo"/></Link>
+       
+      <ul id="navbarSupportedContent">
+        <li>
+          <NavLink activeclassname="active" to="/about">
+            ABOUT
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink activeclassname="active" to="/meat">
+            MEAT
+          </NavLink>
+        </li>
+
+        {/* <li>
+          <NavLink activeclassname="active" to="/buy">
+            BUY
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink activeclassname="active" to="/services">
+            SERVICES
+          </NavLink>
+        </li> */}
+
+        <li>
+          <NavLink activeclassname="active" to="/contact">
+            CONTACT
+          </NavLink>
+        </li>
+
+      </ul>
+      {nav ? (
+        <div className={`your-container ${isSlidIn ? "slide-in" : ""}`}>
+          <ul id="navbarSupportedContentMobile">
+            <li>
+              <Link  to="/">
+                <img src="fkf.jpg" className="logo" alt="logo" />
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/about">
+              <FontAwesomeIcon icon={faCircleInfo} />  ABOUT
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/meat">
+              <FontAwesomeIcon icon={faChartSimple}  className="font"/> MEAT
+              </Link>
+            </li>
+
+            {/* <li>
+              <Link to="/buy">
+              <FontAwesomeIcon icon={faArrowAltCircleUp}  className="font"/> BUY 
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/services">
+              <FontAwesomeIcon icon={faSuitcase} /> SERVICES
+              </Link>
+            </li> */}
+
+            <li>
+              <Link to="/contact">
+              <FontAwesomeIcon icon={faAddressCard} />  CONTACT
+              </Link>
+            </li>
+          </ul>
+        </div>
+      ) : null}
+
+      <div onClick={handleNav} className="zaracho">
+        {nav ? (
+          <FontAwesomeIcon icon={faXmark} className="fas" />
+        ) : (
+          <FontAwesomeIcon icon={faBars} className="fas" />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
